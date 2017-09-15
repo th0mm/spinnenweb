9b389d31e478146089d45bcb5a39881fec176ec2
Carli-B <31988819+Carli-B@users.noreply.github.com>
2017-09-15 14:26:03 +0200

    NN Classes

---
+++ neural_network_test.cpp
@@ -0,0 +1,39 @@
+include <iostream>
+include <vector>
+
+class Sigmoid {
+    private:
+    int layer;
+    int number;
+    float bias;
+    std::vector<int> weights;
+    std::vector<int> inputs;
+    public:
+    void setLocation(int layr, int Number){
+        layer = layr;
+        number = Number;
+    }
+    void setBias(adres){
+        bias = *adres;
+    }
+}
+
+//een class layer
+class Layer{
+    private:
+    int layer;
+    std::vector<Sigmoid *> sigmoids; //sigmoids.push_back(&<naamvansigmoid>)
+    public:
+    void addSigmoid(Sigmoid *sigmoid){
+        sigmoids.push_back(&sigmoid);
+    }
+}
+
+class Network{
+    private:
+    std::vector<Layer *> layers;
+}
+int main(){
+    
+    return 0;
+}
